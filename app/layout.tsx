02c2c1492c95fbb168c32e76ff88b837fe00c1cf
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-11 23:41:26 +0530

    page1 done

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative  h-[5000px]`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative  h-[5000px] pt-28 sm:pt-36`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
         <div className="bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[-35rem] right-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <div></div>
 
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,12 +1,89 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
-    <section>
-        <div className=" flex items-center justify-center">
-            <Image src="https://unsplash.com/photos/mans-grey-and-black-shirt-ILip77SbmOE" alt="Satyajeet Portrait" width="192" height="192" quality="95"/>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="/portrait.png"
+              alt="Satyajeet Portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full border-white border-[0.35rem] object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+            className="absolute bottom-0 right-0 text-4xl"
+          >
+            👋
+          </motion.span>
         </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Satyajeet Gaur.</span> I'm a{" "}
+        <span className="font-bold">software developer</span> pursuing{" "}
+        <span className="font-bold">Master of Computer Application.</span>{" "}I enjoy
+        building <span className="italic">sites & apps</span>. Currently, my focus is {" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#"
+          className="group flex bg-gray-900 text-white px-7 py-3 items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here <BsArrowRight 
+          className="opacity-70 group-hover:translate-x-1 transition"/>
+        </Link>
+        <a className="group flex bg-white rounded-full gap-2 px-7 py-3 items-center outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href="/CV.pdf" download>
+          Downlaod CV <HiDownload className="opacity-70 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a className="bg-white flex p-4 rounded-full items-center gap-2 text-gray-700 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 hover:text-gray-950 transition cursor-pointer border border-black/10" href="https://www.linkedin.com/in/the-satyajeet/" target="_blank">
+          <BsLinkedin />
+        </a>
+
+        <a className="bg-white flex p-4 rounded-full items-center gap-2 text-gray-700 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 hover:text-gray-950 transition cursor-pointer border border-black/10 text-[1.35rem]" href="https://github.com/the-satyajeet" target="_blank">
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
-  )
+  );
 }
