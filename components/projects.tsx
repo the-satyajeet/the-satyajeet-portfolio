ca4e8800417343f0feadca632ef58b66f047f8a1
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-19 23:43:50 +0530

    picture added

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
+import Image from "next/image";
 
 export default function Projects() {
   return (
@@ -23,7 +24,8 @@ type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8">
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] ">
+      <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] ">
       <h3 className="text-2xl font-semibold">{title}</h3>
       <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
       <ul className="flex flex-wrap mt-4 gap-2 ">
@@ -31,7 +33,8 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
           <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>{tag}</li>
         ))} 
       </ul>
-      <img src={imageUrl} alt="Project I worked on" quality={95} />
+      </div>
+      <Image src={imageUrl} alt="Project I worked on" quality={95} className="absolute  top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl "/>
     </section>
   );
 }
