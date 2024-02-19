7297bd1386353c7b7794f63192e103861d8937fd
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-19 23:29:33 +0530

    projects added

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,7 +10,7 @@ export default function Projects() {
         <div>
           {projectsData.map((project, index) => (
             <React.Fragment key={index}>
-              <Project {...Project} />
+              <Project {...project} />
             </React.Fragment>
           ))}
         </div>
@@ -19,9 +19,19 @@ export default function Projects() {
   );
 }
 
-type ProjectProps = (typeof projectsData)[0];
+type ProjectProps = (typeof projectsData)[number];
 
-function Project({ title, description, tags, imageUrl } :
-  ProjectProps) {
-  return <div>{title}</div>;
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return (
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8">
+      <h3 className="text-2xl font-semibold">{title}</h3>
+      <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+      <ul className="flex flex-wrap mt-4 gap-2 ">
+        {tags.map((tag, index) => (
+          <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>{tag}</li>
+        ))} 
+      </ul>
+      <img src={imageUrl} alt="Project I worked on" quality={95} />
+    </section>
+  );
 }
